13164865b7007c00ee28b6099090a92e93b634ad
Milon Chandra Das <milonofficial53@gmail.com>
2024-11-14 12:58:12 +0600

    First Commit

---
+++ File_Input_Output.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("input_custom.txt", "r", stdin); // read from input_custom.txt
+    freopen("output_custom.txt", "w", stdout); // write to output_custom.txt
+    
+   
+    return 0;
+}
---
+++ Template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)x.size()
+#define pf push_front
+#define pb push_back
+#define mp make_pair
+#define nl cout << '\n'
+#define endl '\n'
+#define F first
+#define S second
+
+int main() {
+    optimize();
+    
+    
+    
+    return 0;
+}
