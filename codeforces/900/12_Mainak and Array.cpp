37b715623f5e54f0c4f348abe02304f3ff395840
Milon Chandra Das <milonofficial53@gmail.com>
2025-04-30 22:52:50 +0600

    30 may

--- codeforces/900/12_Mainak and Array.cpp
+++ codeforces/900/12_Mainak and Array.cpp
@@ -14,13 +14,31 @@ constexpr int INF = 1e9;
 #define no cout << "NO" << nl
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) cin >> v[i];
+
+    int ans = INT_MIN;
+    if (n==1) {
+        cout << 0 <<nl;
+        return;
+    }
+
+    for (int i = 1; i < n; i++) ans = max(ans, v[i] - v[0]);
+
+    for (int i = 0; i < n-1; i++) ans = max(ans, v[n-1] - v[i]);
+
+    for (int i = 1; i < n; i++) ans = max(ans, v[i-1] - v[i]);
+    cout << ans << nl;
 }
 
 int main() {
     fastio();
     int t;
     cin >> t;
+
     while (t--) {
         solve();
     }
